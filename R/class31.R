edfcdadb76cb60b75582cf300b62d544b94b5b40
``paul1t0 <``pa666ul@gmailcom>
2015-10-14 20:13:34 -0500

    plot

--- R/class31.R
+++ R/class31.R
@@ -5,5 +5,12 @@ ls()
 # save el script
 # git (arriba)
 # pull Branches (already estamos al dia)
-# comirt
+# comirt (script 31)
+# Push (username: paul1t0) (password: 2011paul)
 
+x<-c(1:5)
+y<-c(5:1)
+plot(x,y)
+
+library(ggplot2)
+require(devtools)
